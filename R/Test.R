0e420c539869e959fb5dca9b11e55a7e8903a997
Dieter Henrik Heiland <dieter.henrik.heiland@unilkinik-freiburg.de>
2021-06-10 11:53:03 +0200

    d

---
+++ R/Test.R
@@ -0,0 +1,16 @@
+#' @title test
+#' @author Dieter Henrik Heiland
+#' @description test
+#' @inherit 
+#' @return 
+#' @examples 
+#' 
+#' @export
+#' 
+test <- function(){
+  
+  print ("test")
+  
+  
+  
+}
